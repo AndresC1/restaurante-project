a062220587a36467110e7bd5c2d7e36ad994e181
AndresC1 <andrescornejolira@gmail.com>
2022-12-21 13:09:08 -0600

    #3 - Creacion del navbar

--- src/page/HomePage.jsx
+++ src/page/HomePage.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
-import useContextDataApp from '../hooks/useContextData'
+import Navbar from '../components/Navbar/navbar'
 
 export default function HomePage() {
-  const contexto = useContextDataApp()
     return (
-    <div>{contexto.hola}</div>
+    <div className='flex items-stretch h-screen bg-[#f7f7f7]'>
+        <Navbar/>
+        <div className='w-full pt-10 pb-28 md:pb-12 md:p-10 px-12 h-full overflow-auto'>
+          
+        </div>
+    </div>
   )
 }
