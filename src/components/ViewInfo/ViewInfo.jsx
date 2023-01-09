5997a0556721234bd82a65deff3061efc9f5b012
AndresC1 <andrescornejolira@gmail.com>
2023-01-09 14:02:11 -0600

    #11 - Modificacion en el diseño de pagina contacto

--- src/components/ViewInfo/ViewInfo.jsx
+++ src/components/ViewInfo/ViewInfo.jsx
@@ -7,19 +7,21 @@ import logo from '../../static/Navigandum.jpg'
 
 export default function ViewInfo() {
   return (
-    <div className='w-full h-auto flex justify-center items-center md:p-10 p-1'>
-        <div className='lg:w-7/12 md:w-10/12 w-full h-auto flex flex-col justify-center space-y-4 items-center py-5 bg-degradado_primario rounded-xl'>
+    <div className='w-full max-w-[65em] h-auto flex justify-center items-center md:p-10 p-1'>
+        <div className='md:w-10/12 w-full h-auto flex flex-col justify-center items-center py-5 bg-degradado_primario rounded-xl'>
             <img className='h-auto w-52 bg-white p-2 rounded-xl' src={logo} alt="Logo" />
-            <div className='lg:w-5/12 md:w-9/12 w-full space-y-3 md:p-3 p-1 flex flex-col items-center justify-center text-gray-600 font-mono font-bold md:text-base text-sm'>
-                <span className='flex w-10/12 justify-start items-center hover:scale-105 transition-all'>
-                    <MdOutlineMailOutline className='h-auto w-6 mr-3'/><h1>correoPrueba@acldev.site</h1>
-                </span>
-                <span className='flex w-10/12 justify-start items-center hover:scale-105 transition-all'>
-                    <FaInstagram className='h-auto w-6 mr-3'/><h1>acldev-site</h1>
-                </span>
-                <span className='flex w-10/12 justify-start items-center hover:scale-105 transition-all'>
-                    <BsTelephone className='h-auto w-6 mr-3'/><h1>(+505) 8888-8888</h1>
-                </span>
+            <div className='lg:w-5/12 md:w-9/12 w-full md:p-3 p-1 flex flex-col items-center justify-center text-gray-600 font-mono font-bold md:text-base text-sm'>
+                <div className='w-auto mt-3'>
+                    <span className='w-full h-auto flex justify-start items-center my-1 hover:scale-105 transition-all'>
+                        <MdOutlineMailOutline className='h-auto w-6 mr-3'/><h1>correoPrueba@acldev.site</h1>
+                    </span>
+                    <span className='w-full h-auto flex justify-start items-center my-1 hover:scale-105 transition-all'>
+                        <FaInstagram className='h-auto w-6 mr-3'/><h1>acldev-site</h1>
+                    </span>
+                    <span className='w-full h-auto flex justify-start items-center my-1 hover:scale-105 transition-all'>
+                        <BsTelephone className='h-auto w-6 mr-3'/><h1>(+505) 8888-8888</h1>
+                    </span>
+                </div>
             </div>
         </div>
     </div>
